0dd756e6c92059d8b98acf5a44bbee0f3e0e552d
Kaissés Sedrês <kaisses.s@gmail.com>
2025-05-04 13:07:00 +0000

    test

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// pages/_app.js
-import "../index.css"; // Agora você pode importar o CSS global aqui
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
