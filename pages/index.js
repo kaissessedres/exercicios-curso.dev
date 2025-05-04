2f8ef23c8f0623329d693126a0bb37ebd45e32ac
Kaissés Sedrês <kaisses.s@gmail.com>
2025-05-04 12:33:12 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ const Home = () => {
       <h1>💖 Eu te amo, Mariana 💖</h1>
       <div className="gif">
         <img
-          src="https://media.giphy.com/media/Q7ozWVYCR0nyW2rvPW/giphy.gif"
-          alt="Totoro Studio Ghibli"
+          src="https://media.giphy.com/media/Q7ozWVYCR0nyW2rvPW/https://64.media.tumblr.com/a2bfa8a583f38db3fb6c8d377d74cb3f/tumblr_n9ljxwShPJ1shdhdjo1_500.gif"
+          alt="Studio Ghibli"
           width="250"
         />
       </div>
